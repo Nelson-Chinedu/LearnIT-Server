e8591c22bdcf7f93c128f08bdbc0783e9e7c306b
Nelson Chinedu <egwuchinedu69@gmail.com>
2022-04-29 06:29:46 +0100

    add API doc

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,10 +1,21 @@
 import express from 'express';
+import swaggerJsDoc from 'swagger-jsdoc';
+import { serve, setup } from 'swagger-ui-express';
+import swaggerDefinition from '../../doc/api-specification';
 import authRoute from './auth';
 import userRoute from './user';
 
+const specs = swaggerJsDoc(swaggerDefinition);
 const router = express.Router();
 const prefix = '/api/v1';
+const apiDocs = '/api/docs';
+const specsConfig = setup(specs, {
+  explorer: false,
+  customSiteTitle: 'LearnIT',
+});
 
+router.use(apiDocs, serve);
+router.use(apiDocs, specsConfig);
 router.use(prefix, authRoute);
 router.use(prefix, userRoute);
 
--- src/routes/api/user.ts
+++ src/routes/api/user.ts
@@ -15,15 +15,187 @@ import videoUpload from '../../controller/videoUpload';
 
 const router = express.Router();
 
+/**
+ * @swagger
+ *
+ * /user/me:
+ *  get:
+ *    summary: User profile
+ *    description: Get user profile
+ *    tags:
+ *      - Users
+ *    responses:
+ *      200:
+ *        description: user profile
+ *      401:
+ *        description: unauthorized
+ */
 router.get('/user/me', authentication, getProfileController);
+
+/**
+ * @swagger
+ *
+ * /user/me:
+ *  put:
+ *    summary: User profile
+ *    description: Update user profile
+ *    tags:
+ *      - Users
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              firstname:
+ *                type: string
+ *              lastname:
+ *                 type: string
+ *              phone:
+ *                 type: string
+ *              city:
+ *                 type: string
+ *              state:
+ *                 type: string
+ *              zipCode:
+ *                 type: string
+ *              address:
+ *                 type: string
+ *              country:
+ *                 type: string
+ *    responses:
+ *      204:
+ *        description: successfull
+ *      401:
+ *        description: unauthorized
+ *      400:
+ *        description: bad request
+ */
 router.put('/user/me', authentication, profileValidator, updateProfile);
+
+/**
+ * @swagger
+ *
+ * /user/me/bio:
+ *  put:
+ *    summary: User bio
+ *    description: update user bio
+ *    tags:
+ *      - Users
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              mentorBio:
+ *                type: string
+ *    responses:
+ *      204:
+ *        description: successfull
+ *      401:
+ *        description: unauthorized
+ *      400:
+ *        description: bad request
+ *
+ */
 router.put('/user/me/bio', authentication, mentorBioValidator, updateBio);
+
+/**
+ * @swagger
+ *
+ * /upload/video:
+ *  post:
+ *    summary: course video
+ *    description: user (mentor) can upload course video
+ *    tags:
+ *      - Users
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        multipart/form-data:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              video:
+ *                type: string
+ *                format: binary
+ *    produces:
+ *      application/json:
+ *        schema:
+ *          type: object
+ *          properties:
+ *            status:
+ *              type: number
+ *            message:
+ *              type: string
+ *            payload:
+ *              type: object
+ *              properties:
+ *                url:
+ *                  type: string
+ *    responses:
+ *      201:
+ *        description: successfull
+ *      403:
+ *        description: forbidden
+ *      401:
+ *        description: unauthorized
+ */
 router.post(
   '/upload/video',
   authentication,
   UserMiddleware.findRole,
   videoUpload
 );
+
+/**
+ * @swagger
+ *
+ * /add/course:
+ *  post:
+ *    summary: add course
+ *    description: user (mentor) can add course
+ *    tags:
+ *      - Users
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              course_name:
+ *                type: string
+ *              price:
+ *                type: string
+ *              video_url:
+ *                type: array
+ *                items:
+ *                  type: string
+ *    produces:
+ *      application/json:
+ *        schema:
+ *          type: object
+ *          properties:
+ *            status:
+ *              type: number
+ *            message:
+ *              type: string
+ *            payload:
+ *              type: object
+ *    responses:
+ *      201:
+ *        description: successfull
+ *      401:
+ *        description: unauthorized
+ *      403:
+ *        description: forbidden
+ *      400:
+ *        description: bad request
+ */
 router.post(
   '/add/course',
   authentication,
