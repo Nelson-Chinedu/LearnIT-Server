502f2db22f8d01053b37563a7f5e5ad7102379ad
Nelson Chinedu <egwuchinedu69@gmail.com>
2023-01-11 12:54:19 +0100

    add change

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,11 @@
 import winstonEnvLogger from 'winston-env-logger';
 import app from './index';
 
-// const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
-app.listen(process.env.PORT || 8080, () => {
+app.listen(PORT, () => {
   winstonEnvLogger.info({
-    message: `Server started on port ${process.env.PORT || 8080}`,
+    message: `Server started on port ${PORT}`,
   });
   winstonEnvLogger.info({
     message: `Press CTRL + C to stop Server`,
